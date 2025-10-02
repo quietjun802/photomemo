80adaa4f163963366573832a62c6669d936d69a2
quietjun802 <whdydwns802@gmail.com>
2025-10-02 19:37:52 +0900

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+
+import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import AuthPanel from './components/AuthPanel'
 import Landing from './pages/Landing'
-import './App.scss'
 function App() {
+
   return (
     <div className='page'>
       <Routes>
         <Route path='/' element={<Landing />} />
-        
+        <Route path='/admin/login' element={<AuthPanel />} />
       </Routes>
     </div>
   )
 }
-
 
 export default App
